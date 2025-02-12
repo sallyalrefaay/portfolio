9bd7788de9e151e60b4247fd7521e19109e6dd74
Sally Alrefaay <sallyalrefaay@gmail.com>
2025-02-12 12:03:13 +0300

    last update

--- src/Component/Portfolio/Portfolio.jsx
+++ src/Component/Portfolio/Portfolio.jsx
@@ -10,68 +10,68 @@ export default function Portfolio() {
             <button><a href="https://github.com/sallyalrefaay/" target="_blank">View GitHub <i className="fa-solid fa-up-right-from-square"></i></a></button>
         </div>
         <div className="projects">
-            <div className="project pro-1">
+            <div className="project pro">
+                <img src="/portfolio/assets/image/project-1.png" alt="" />
                 <div className="bgColor">
                     <h2>K2skiing</h2>
                     <Link to= '/Project'>
                     <div>
                         <button><i className="fa-solid fa-up-right-from-square"></i></button><span>More Details</span>
-                        <div><a href="https://sallyalrefaay.github.io/K2skiing/" target="_blank"><button><i className="fas fa-eye"></i></button>Project Demo</a></div>
                     </div>
                     </Link>
                 </div>
             </div>
-            <div className="project pro-2">
+            <div className="project pro">
+            <img src="/portfolio/assets/image/project-2.png" alt="" />
                 <div className="bgColor">
                     <h2>Flora</h2>
                     <Link to= '/Project'>
                     <div>
                         <button><i className="fa-solid fa-up-right-from-square"></i></button> <span>More Details</span>
-                        <div><a href="https://sallyalrefaay.github.io/Flora/" target="_blank"><button><i className="fas fa-eye"></i></button>Project Demo</a></div>
                     </div>
                     </Link>
                 </div>
             </div>
-            <div className="project pro-3">
+            <div className="project pro">
+            <img src="/portfolio/assets/image/project-3.png" alt="" />
                 <div className="bgColor">
                     <h2>Salla Store</h2>
                     <Link to= '/Project'>
                     <div>
                         <button><i className="fa-solid fa-up-right-from-square"></i></button><span>More Details</span>
-                        <div><a href="https://sallyalrefaay.github.io/Salla-store/" target="_blank"><button><i className="fas fa-eye"></i></button>Project Demo</a></div>
                     </div>
                     </Link>
                 </div>
             </div>
-            <div className="project pro-4">
+            <div className="project pro">
+            <img src="/portfolio/assets/image/project-4.png" alt="" />
                 <div className="bgColor">
                     <h2>Edujar</h2>
                     <Link to= '/Project'>
                     <div>
                         <button><i className="fa-solid fa-up-right-from-square"></i></button><span>More Details</span>
-                        <div><a href="https://sallyalrefaay.github.io/Edujar/" target="_blank"><button><i className="fas fa-eye"></i></button>Project Demo</a></div>
                     </div>
                     </Link>
                 </div>
             </div>
-            <div className="project pro-5">
+            <div className="project pro">
+            <img src="/portfolio/assets/image/project-5.png" alt="" />
                 <div className="bgColor">
                     <h2>DashStack</h2>
                     <Link to= '/Project'>
                     <div>
                         <button><i className="fa-solid fa-up-right-from-square"></i></button><span>More Details</span>
-                        <div><a href="https://sallyalrefaay.github.io/DashStack/" target="_blank"><button><i className="fas fa-eye"></i></button>Project Demo</a></div>
                     </div>
                     </Link>
                 </div>
             </div>
-            <div className="project pro-6">
+            <div className="project pro">
+            <img src="/portfolio/assets/image/project-6.png" alt="" />
                 <div className="bgColor">
                     <h2>Home Decoration</h2>
                     <Link to= '/Project'>
                     <div>
                         <button><i className="fa-solid fa-up-right-from-square"></i></button><span>More Details</span>
-                        <div><a href="https://sallyalrefaay.github.io/Home-Decoration/" target="_blank"><button><i className="fas fa-eye"></i></button>Project Demo</a></div>
                     </div>
                     </Link>
                 </div>
